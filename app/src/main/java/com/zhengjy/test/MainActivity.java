b52a3d1c72c12e7d8ef5c3e60cd5b6a9cb70c2a7
zhengjy <zhengjy@yealink.com>
2017-07-13 15:04:58 +0800

    add BadgeUtils

--- app/src/main/java/com/zhengjy/test/MainActivity.java
+++ app/src/main/java/com/zhengjy/test/MainActivity.java
@@ -2,8 +2,8 @@ package com.zhengjy.test;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
 
